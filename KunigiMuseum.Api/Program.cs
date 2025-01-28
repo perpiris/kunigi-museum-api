7626b16c7d94656608d2a925c17d1dfd188f0703
iperp <iperpirakis@gmail.com>
2025-01-28 22:55:02 +0200

    added cors

--- KunigiMuseum.Api/Program.cs
+++ KunigiMuseum.Api/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddApplication(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
